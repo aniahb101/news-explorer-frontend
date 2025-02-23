143e5217d367a4f47af254b235f0b78f59f9ee24
aniahb <aniah6767@gmail.com>
2025-02-22 23:34:24 -0500

    Fix Vite GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/news-explorer-frontend/",
+  base: "/news-explorer-frontend/", // 👈 Must match your GitHub repo name exactly!
   plugins: [react()],
   server: {
     port: 3000,
